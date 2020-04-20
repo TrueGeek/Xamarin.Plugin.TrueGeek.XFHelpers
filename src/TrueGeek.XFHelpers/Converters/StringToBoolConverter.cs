625a095a4d254ce00064725df0246b19fd21e017
Brad Dean <Brad@TrueGeek.com>
2020-04-20 16:11:00 -0400

    Added StringToBool converter

---
+++ src/TrueGeek.XFHelpers/Converters/StringToBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TrueGeek.XFHelpers.Converters
+{
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !string.IsNullOrEmpty((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return string.Empty;
+        }
+    }
+}
